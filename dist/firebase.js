435fdd94e9fd46fd3b1c30f2124f43e693e06fc8
anis-king-911 <91683396+anis-king-911@users.noreply.github.com>
2022-09-17 18:28:49 +0100

    Add files via upload

---
+++ dist/firebase.js
@@ -0,0 +1,168 @@
+const firebaseVersion = '9.9.4';
+
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+import {
+  getDatabase, ref, child, onValue,
+  set, push, update, remove,
+  query, orderByChild, limitToLast,
+  endBefore, startAfter, limitToFirst
+} from "https://www.gstatic.com/firebasejs/9.9.4/firebase-database.js";
+import {
+  getStorage, ref as cloud, listAll,
+  uploadBytesResumable, getDownloadURL,
+  deleteObject
+} from "https://www.gstatic.com/firebasejs/9.9.4/firebase-storage.js";
+import {
+  getAuth, signInWithEmailAndPassword,
+  onAuthStateChanged, signOut
+} from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwI_lwV52VuKJYjeSID811WEv5u2AF70w",
+  authDomain: "manga4up-vercel.firebaseapp.com",
+  databaseURL: "https://manga4up-vercel-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "manga4up-vercel",
+  storageBucket: "manga4up-vercel.appspot.com",
+  messagingSenderId: "1063989292418",
+  appId: "1:1063989292418:web:427fb5e5422fc4858bf39b"
+}
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
+
+let Manga4Up = 'Manga4Up/', List = 'List/', size = 3;
+
+export {
+  database, storage, Manga4Up, List, size, Mangalist, UpComming, Options,
+  ref, child, onValue, set, push, update, remove, 
+  query, orderByChild, limitToLast, endBefore, startAfter, limitToFirst,
+  cloud, listAll, getDownloadURL, deleteObject,
+  uploadBytesResumable,
+  
+  auth, signInWithEmailAndPassword, signOut,
+  onAuthStateChanged
+}
+
+export function GetMangaList(form) {
+  const databaseRef = ref(database, List);
+  
+  onValue(databaseRef, (snapshot)=> {
+    snapshot.forEach((snap)=> {
+      const data = snap.val();
+      
+      form.querySelector('select').innerHTML +=
+        `<option value="${data.Title}">${data.Title}</option>`;
+    })
+  })
+}
+export function MangaSelectionList(container) {
+  const databaseRef = ref(database, List);
+  
+  onValue(databaseRef, (snapshot)=> {
+    snapshot.forEach((snap)=> {
+      const data = snap.val();
+      
+      container.innerHTML +=
+        `<option value="${data.Title}">${data.Title}</option>`;
+    })
+  })
+}
+//export 
+
+export function GetID(form) {
+  const databaseRef = ref(database, Manga4Up);
+  const databaseLimit = query(databaseRef, limitToLast(1));
+
+  onValue(databaseLimit, (snapshot)=> {
+    form.querySelector('#VolumeID').innerHTML = ''
+    snapshot.forEach((snap)=> {
+      const value = snap.val();
+      const data = value['Volume Data'];
+      
+      form.querySelector('#VolumeID').innerHTML = `volume id is going to be [ <em>${data.ID+1}</em> ]`;
+    })
+  })
+}
+
+
+export const insert = (arr, index, newItem) => [
+  ...arr.slice(0, index),
+  newItem,
+  ...arr.slice(index)
+]
+
+
+export function TableSearch() {
+  const inpValue = document.querySelector('.SearchByTitle').value.toUpperCase();
+  const tableRows = document.querySelectorAll('.EditList tr');
+  
+  tableRows.forEach((row) => {
+    const cell = row.querySelectorAll('td')[1];
+    const span = cell.querySelector('span');
+    
+    if (span) {
+      const title = span.textContent || span.innerText;
+      if (title.toUpperCase().indexOf(inpValue) > -1) {
+        row.style.display = "";
+      } else {
+        row.style.display = "none";
+      }
+    }
+  })
+}
+
+export function getOptions(form) {
+  const TagsOutput = [];
+  for (let i = 0; i < form.length; i++) {
+    if (form[i].checked) {
+      TagsOutput.push(form[i].name);
+    }
+  }
+  return TagsOutput;
+}
+
+
+const UpComming = [
+  '',
+  'Horimiya',
+  '',
+  'Kaguya-sama: Love is War',
+  'Pumpkin Night',
+  'Golem Hearts',
+];
+const Mangalist = [
+  '',
+  'Tate No Yuusha No Nariagari',
+  'Golden Kamuy',
+  'Vagabond',
+  'Spy X Family',
+  'Kaiju No. 8',
+  'Horimiya',
+//  'Kaguya-sama: Love is War',
+  '',
+
+  'Ao No Exorcist',
+  'Black Clover',
+  'Boku No Hero Academia',
+  'Boruto NNG',
+  'Dragon Ball Super',
+  'Edens Zero',
+  'Fairy Tail 100 Years Quest',
+  'Jujutsu Kaisen',
+  "Komi Can't Communicate",
+  'Made in Abyss',
+  'Noragami',
+  'One Punch Man',
+  'Record Of Ragnarok',
+  'The Four Knights of Apocalypse',
+  'Tokyo Revengers',
+  'Vinland Saga',
+];
+
+const Options = [
+  'Comming Soon', '+18', 'One Shot',
+];
---
+++ dist/main.js
@@ -0,0 +1,294 @@
+import {
+  Mangalist, UpComming, GetMangaList,
+  MangaSelectionList, insert,
+  GetID, TableSearch, Options, getOptions,
+} from '/dist/firebase.js';
+
+const WindowPATH = window.location.pathname;
+const WindowMODE = window.location.href.split('#')[1];
+const WindowREF = window.location.href.split('/').pop();
+
+const SignInForm = document.querySelector('.SignIn');
+const UserContainer = document.querySelector('.UserContainer');
+
+const UploadVolume = document.querySelector('.UploadVolume');
+const UploadManga = document.querySelector('.UploadManga');
+const UploadBlog = document.querySelector('.UploadBlog');
+
+const EditRecent = document.querySelector('.EditRecent');
+const EditList = document.querySelector('.EditList');
+const EditBlogs = document.querySelector('.EditBlogs')
+
+const UpdateVolume = document.querySelector('.UpdateVolume');
+const UpdateManga = document.querySelector('.UpdateManga');
+const UpdateBlog = document.querySelector('.UpdateBlog');
+
+const SelectManga = document.querySelector('#SelectManga');
+const SoloManga = document.querySelector('.SoloManga');
+
+const SearchByTitle = document.querySelector('.SearchByTitle');
+const LoadMore = document.querySelector('.LMP');
+const LoadLess = document.querySelector('.LLP');
+
+window.onload = ()=> {
+  if(WindowPATH === '/index.html' || WindowPATH === '/') {
+
+    GetID(UploadVolume)
+
+    SignInForm.addEventListener('submit', async (event)=> {
+      event.preventDefault();
+      
+      const data = {
+        email: SignInForm.email.value,
+        password: SignInForm.password.value
+      }
+      
+      const { SignIn } = await import('/dist/src/Authentication.js');
+      
+      SignIn(data);
+      setTimeout(()=> {SignInForm.reset()}, 600);
+    })
+    
+    UploadVolume.addEventListener('submit', async (event)=> {
+      event.preventDefault();
+
+      let UrlArray =  UploadVolume.VolumeCover.value;
+      let NewUrl = insert(UrlArray.split('/'), 4, 'tr:w-150').join('/');
+      
+      const data = {
+        'ID': UploadVolume.querySelector('em').textContent,
+        'Manga Title': UploadVolume.MangaTitle.value,
+        'Volume Number': UploadVolume.VolumeNumber.value,
+        'Volume Cover': NewUrl,
+        'File Size': UploadVolume.FileSize.value,
+        'Chapters Count': {
+          From: UploadVolume.FromChapter.value,
+          To: UploadVolume.ToChapter.value,
+        },
+        'Download Links': [
+          UploadVolume.DLink1.value,
+          UploadVolume.DLink2.value
+        ],
+        'CreatedAt': Number(Date.now())
+      }
+      
+      const { UploadVolumeFunc } = await import('/dist/src/Upload.js');
+      
+      UploadVolumeFunc(data);
+      setTimeout(()=> {UploadVolume.reset()}, 600);
+    })
+    
+    UploadManga.addEventListener('submit', async (event)=> {
+      event.preventDefault();
+      
+      let UrlArray =  UploadManga.LastCover.value;
+      let NewUrl = insert(UrlArray.split('/'), 4, 'tr:w-150').join('/');
+      
+      const data = {
+        'Manga Title': UploadManga.MangaTitle.value,
+        'Manga State': UploadManga.MangaState.value,
+        'Volumes Count': UploadManga.VolumesCount.value,
+        'Last Volume': NewUrl,
+        'Options': getOptions(UploadManga),
+        'CreatedAt': Number(Date.now())
+      }
+      
+      const { UploadMangaFunc } = await import('/dist/src/Upload.js');
+
+      UploadMangaFunc(data);
+      setTimeout(()=> {UploadManga.reset()}, 600);
+    })
+    
+    UploadBlog.addEventListener('submit', async (event) => {
+      event.preventDefault();
+      
+     const ProgressBar = UploadBlog.querySelector('#progress');
+      
+      const data = {
+        Title: UploadBlog.BlogTitle.value,
+        Content: UploadBlog.BlogContent.value,
+        Cover: UploadBlog.BlogCover.files[0],
+        CreatedAt: Number(Date.now())
+      }
+      
+      const { UploadBlogFunc } = await import('/dist/src/Upload.js');
+
+      UploadBlogFunc(data, ProgressBar);
+      setTimeout(()=> {UploadBlog.reset()}, 600);
+    })
+    
+    import('/dist/src/Retrieve.js').then((modules) => {
+      const {
+        RetrieveRecent, RetrieveList, RetrieveBlogs
+      } = modules;
+
+      RetrieveRecent(EditRecent);
+      RetrieveList(EditList);
+      RetrieveBlogs(EditBlogs);
+    })
+    
+    import('/dist/src/Authentication.js').then((modules) => {
+      const { AuthState } = modules;
+      
+      AuthState(UserContainer, SignInForm);
+    })
+    
+    Mangalist.map(manga => {
+      UploadVolume.MangaTitle.innerHTML +=
+        `<option value="${manga}">${manga}</option>`;
+    })
+
+    UpComming.map(newManga => {
+      UploadManga.MangaTitle.innerHTML +=
+        `<option value="${newManga}">${newManga}</option>`;
+    })
+    
+    Options.map(Option => {
+      UploadManga.querySelector('.Options').innerHTML +=
+        `
+      <label class="choice">
+        <input type="checkbox" name="${Option}" />
+        <span>${Option}</span>
+      </label>
+        `;
+    })
+    
+    SelectManga.addEventListener('change', async (event)=> {
+      const { RetrieveByTitle } = await import('/dist/src/Retrieve.js');
+      RetrieveByTitle(SoloManga, event.target.value);
+    })
+    
+    SearchByTitle.addEventListener('keyup', () => {
+      TableSearch();
+    })
+    
+    /* Pagination Start */
+
+    LoadMore.addEventListener('click', async () => {
+      const { RetrieveLoadMore } = await import('/dist/src/Retrieve.js');
+      RetrieveLoadMore(EditRecent);
+    })
+    LoadLess.addEventListener('click', async () => {
+      const { RetrieveLoadLess } = await import('/dist/src/Retrieve.js');
+      RetrieveLoadLess(EditRecent);
+    })
+
+    /* Pagination End */
+    
+    MangaSelectionList(SelectManga);
+  } else if(WindowPATH === '/edit.html' && WindowMODE === '/recent') {
+    
+    UpdateManga.style.display = 'none';
+    UpdateBlog.style.display = 'none';
+    GetMangaList(UpdateVolume);
+    
+    import('/dist/src/Edit.js').then((modules) => {
+      const { GetVolumeData } = modules;
+      
+      GetVolumeData(WindowREF, UpdateVolume);
+    })
+    
+    UpdateVolume.addEventListener('submit', async (event)=> {
+      event.preventDefault();
+      let NewUrl;
+
+      if(UpdateVolume.VolumeCover.value.match('tr:w-150') === null) {
+        let OldUrl =  UpdateVolume.VolumeCover.value;
+        NewUrl = insert(OldUrl.split('/'), 4, 'tr:w-150').join('/');
+      } else {
+        NewUrl = UpdateVolume.VolumeCover.value;
+      }
+
+      const data = {
+        'Manga Title': UpdateVolume.MangaTitle.value,
+        'Volume Number': UpdateVolume.VolumeNumber.value,
+        'Volume Cover': NewUrl,
+        'File Size': UpdateVolume.FileSize.value,
+        'Chapters Count': {
+          From: UpdateVolume.FromChapter.value,
+          To: UpdateVolume.ToChapter.value,
+        },
+        'Download Links': [
+          UpdateVolume.DLink1.value,
+          UpdateVolume.DLink2.value
+        ],
+      };
+      
+      const { UpdateVolumeFunc } = await import('/dist/src/Update.js');
+      UpdateVolumeFunc(WindowREF, data);
+    })
+    
+  } else if(WindowPATH === '/edit.html' && WindowMODE === '/list') {
+    
+    UpdateVolume.style.display = 'none';
+    UpdateBlog.style.display = 'none';
+    GetMangaList(UpdateManga);
+    
+    import('/dist/src/Edit.js').then((modules) => {
+      const { GetMangaData } = modules;
+      
+      GetMangaData(WindowREF, UpdateManga);
+    })
+    
+    Options.map(Option => {
+      UpdateManga.querySelector('.Options').innerHTML +=
+        `
+      <label class="choice">
+        <input type="checkbox" name="${Option}" />
+        <span>${Option}</span>
+      </label>
+        `;
+    })
+    
+    UpdateManga.addEventListener('submit', async (event)=> {
+      event.preventDefault();
+      let NewUrl;
+
+      if(UpdateManga.LastCover.value.match('tr:w-150') === null) {
+        let OldUrl = UpdateManga.LastCover.value;
+        NewUrl = insert(OldUrl.split('/'), 4, 'tr:w-150').join('/');
+      } else {
+        NewUrl = UpdateManga.LastCover.value;
+      }
+      
+      const data = {
+        'Manga Title': UpdateManga.MangaTitle.value,
+        'Manga State': UpdateManga.MangaState.value,
+        'Volumes Count': UpdateManga.VolumesCount.value,
+        'Options': getOptions(UpdateManga),
+        'Last Volume': NewUrl,
+      };
+      
+      const { UpdateMangaFunc } = await import('/dist/src/Update.js');
+      UpdateMangaFunc(WindowREF, data);
+    })
+    
+  } else if(WindowPATH === '/edit.html' && WindowMODE === '/blog') {
+    
+    UpdateVolume.style.display = 'none';
+    UpdateManga.style.display = 'none';
+    
+    import('/dist/src/Edit.js').then((modules) => {
+      const { GetBlogData } = modules;
+      
+      GetBlogData(WindowREF, UpdateBlog);
+    })
+    
+    UpdateBlog.addEventListener('submit', async (event) => {
+      event.preventDefault();
+      
+      const ProgressBar = UpdateBlog.querySelector('#progress');
+      
+      const data = {
+        Title: UpdateBlog.BlogTitle.value,
+        Content: UpdateBlog.BlogContent.value,
+        Cover: UpdateBlog.BlogCover.files[0],
+        UpdatedAt: Number(Date.now())
+      }
+      
+      const { UpdateBlogFunc } = await import('/dist/src/Update.js');
+      UpdateBlogFunc(WindowREF, data, ProgressBar);
+    })
+    
+  }
+}
